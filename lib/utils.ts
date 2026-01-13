28ec19dc166311ed9a4c782f25a70c5879fe0636
Bruno-Signori <brunowsignori@gmail.com>
2026-01-12 21:32:25 -0300

    teste para subir

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+// Utility function to format a date to 'YYYY-MM-DD' format
